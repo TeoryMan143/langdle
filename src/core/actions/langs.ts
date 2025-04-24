5fb533a7e4a9aa119ac9adb5e56a6fbc2d2f53f2
Jonathan <jonathancortestm143@gmail.com>
2025-04-24 13:52:59 -0500

    refactor: move language feching functions

--- src/core/actions/langs.ts
+++ src/core/actions/langs.ts
@@ -1,8 +1,9 @@
 'use server';
 
 import { actionError, ActionResult, actionSuccess } from '@/core/actions/utils';
-import { Language, LanguageCode } from '@/core/lib/types';
+import type { Language, LanguageCode, LanguageData } from '@/core/lib/types';
 import { LANG_API_URL } from '@/core/lib/utils';
+import { revalidatePath } from 'next/cache';
 
 export async function getAllLanguages(): Promise<ActionResult<Language[]>> {
   try {
@@ -37,6 +38,35 @@ export async function getLanguage(
     }
 
     const lang = (await res.json()) as Language;
+
+    return actionSuccess(lang);
+  } catch (e) {
+    let message = 'unknown error';
+    if (e instanceof Error) {
+      message = e.message;
+    }
+    return actionError(message);
+  }
+}
+
+export async function setLanguageData(
+  code: LanguageCode,
+  data: LanguageData,
+): Promise<ActionResult<string>> {
+  try {
+    const res = await fetch(`${LANG_API_URL}/api/lang/${code}`, {
+      method: 'put',
+      body: JSON.stringify(data),
+    });
+
+    if (res.status !== 200) {
+      const error = await res.json();
+      return actionError(error.message);
+    }
+
+    revalidatePath(`/data/${code}`);
+
+    const lang = (await res.json()).message;
 
     return actionSuccess(lang);
   } catch (e) {
