330f83c95e31efadb8199b16b48c6aeaa63a7bab
Jonathan <jonathancortestm143@gmail.com>
2025-04-24 07:56:25 -0500

    feat: add Toaster component for notifications in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Header from '@/core/components/main/header';
 import { noto } from '@/core/lib/fonts';
+import { Toaster } from 'sonner';
 
 export const metadata: Metadata = {
   title: 'Langdle',
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${noto.className} antialiased flex flex-col min-h-dvh`}>
+        <Toaster />
         <Header />
         <div className='flex-1 max-w-4xl mx-auto py-3'>{children}</div>
       </body>
