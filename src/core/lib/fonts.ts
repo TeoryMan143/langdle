1002a372fcc71a84f8ce6ba45cc90a7ee456758c
teoryman143 <jonathancortestm143@gmail.com>
2025-04-30 19:00:36 -0500

    fix: ensure preload is set to false for Noto Sans font

--- src/core/lib/fonts.ts
+++ src/core/lib/fonts.ts
@@ -3,6 +3,7 @@ import { Noto_Sans, Dongle } from 'next/font/google';
 export const noto = Noto_Sans({
   variable: '--font-noto',
   weight: ['200', '300', '400', '500', '600', '700'],
+  preload: false,
 });
 
 export const dongle = Dongle({
