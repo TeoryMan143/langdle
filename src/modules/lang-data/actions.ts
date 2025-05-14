ce920d902086818c90516c381db35bb7589ea6ed
Jonathan <jonathancortestm143@gmail.com>
2025-05-14 13:36:25 -0500

    feat(langPermissions): implement getLangPermissions function to fetch user permissions

--- src/modules/lang-data/actions.ts
+++ src/modules/lang-data/actions.ts
@@ -1,3 +1,21 @@
 'use server';
 
-export async function getLangPermissions() {}
+import { actionError, ActionResult, actionSuccess } from '@/core/actions/utils';
+
+export async function getLangPermissions(
+  userId: string,
+): Promise<ActionResult<string[]>> {
+  try {
+    const permissions = (await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/lang/permissions/${userId}`,
+    ).then(res => res.json())) as string[];
+    return actionSuccess(permissions);
+  } catch (e) {
+    console.error(e);
+    let message = 'unknown';
+    if (e instanceof Error) {
+      message = e.message;
+    }
+    return actionError(message);
+  }
+}
