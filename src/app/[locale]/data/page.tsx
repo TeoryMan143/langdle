ec309536aa2bc73b9952d6c88ad17477295fbf7b
Jonathan <jonathancortestm143@gmail.com>
2025-05-16 09:40:31 -0500

    feat(data-page): implement authentication and permissions check for user access

--- src/app/[locale]/data/page.tsx
+++ src/app/[locale]/data/page.tsx
@@ -1,7 +1,29 @@
 import { dongle } from '@/core/lib/fonts';
+import { auth } from '@/modules/auth/actions';
+import { getLangPermissions } from '@/modules/lang-data/actions';
 import LangsList from '@/modules/lang-data/components/langs-list';
+import { redirect, RedirectType } from 'next/navigation';
 
-function DataPage() {
+async function DataPage() {
+  const { session, user } = await auth();
+
+  if (!session) {
+    redirect('/', RedirectType.replace);
+  }
+
+  const permissionsRes = await getLangPermissions(user.id);
+
+  if (!permissionsRes.success) {
+    console.error(permissionsRes.error);
+    redirect('/', RedirectType.replace);
+  }
+
+  const permissions = permissionsRes.result;
+
+  if (permissions.length === 0 && !user.admin) {
+    redirect('/', RedirectType.replace);
+  }
+
   return (
     <main className='mt-5'>
       <h2 className={`${dongle.className} text-6xl mb-5 text-center`}>
