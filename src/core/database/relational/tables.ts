21e74e7698f3d1b757f5bddd8a266ce30003342b
teoryman143 <jonathancortestm143@gmail.com>
2025-05-20 20:54:39 -0500

    fix(database): update foreign key constraint for session table to include onDelete cascade

--- src/core/database/relational/tables.ts
+++ src/core/database/relational/tables.ts
@@ -18,7 +18,7 @@ export const sessionTable = pgTable('session', {
   id: text('id').primaryKey(),
   userId: uuid('user_id')
     .notNull()
-    .references(() => userTable.id),
+    .references(() => userTable.id, { onDelete: 'cascade' }),
   expiresAt: timestamp('expires_at', {
     withTimezone: true,
     mode: 'date',
