33c88709f306c4d5155b120d12540468990dc3b8
teoryman143 <jonathancortestm143@gmail.com>
2025-06-08 15:28:56 -0500

    feat: add image to langs and extract lang selector component

--- src/modules/lang-data/components/langs-list.tsx
+++ src/modules/lang-data/components/langs-list.tsx
@@ -1,5 +1,5 @@
 import { getAllLanguages, getLanguagesByIds } from '@/core/actions/langs';
-import { Link } from '@/i18n/navigation';
+import LangSelector from './lang-selector';
 
 async function LangsList({ allowedLangs }: { allowedLangs?: string[] }) {
   const langsRes = !allowedLangs
@@ -19,21 +19,6 @@ async function LangsList({ allowedLangs }: { allowedLangs?: string[] }) {
         <LangSelector key={id} code={id} name={name} exonym={exonym} />
       ))}
     </div>
-  );
-}
-
-function LangSelector({
-  code,
-  name,
-  exonym,
-}: { code: string; name: string; exonym?: string }) {
-  return (
-    <Link
-      href={`/data/${code}`}
-      className='py-3 border-b border-black block transition-colors px-4 hover:bg-main'
-    >
-      <p className='text-center text-xl'>{`${'🇺🇸'} ${name}${exonym ? ` (${exonym})` : ''}`}</p>
-    </Link>
   );
 }
 
